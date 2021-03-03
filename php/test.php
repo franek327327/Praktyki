e95ea9401eeabda577e9f81eb18e34d76a7d3127
woodman0 <mlamlok@interia.pl>
2021-03-04 00:33:30 +0100

    nic

--- php/test.php
+++ php/test.php
@@ -7,6 +7,7 @@ $polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
 $sql = 'SELECT * FROM plan';
 $wynik = mysqli_query($polaczenie, $sql);
 $plan = mysqli_fetch_all($wynik, MYSQLI_ASSOC);
+print_r($plan);
 mysqli_free_result($wynik);
 $polaczenie->close();
 ?>
