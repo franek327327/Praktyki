00e005dd9854d08e1c4dd29ffe8812b5046aa010
woodman0 <mlamlok@interia.pl>
2021-03-04 18:37:33 +0100

    edgf

--- php/plan.php
+++ php/plan.php
@@ -8,7 +8,7 @@ $polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
 $lekcja = 
 "SELECT slownik.przedmiot, dni.dzien, godzinylekcyjne.godzina, sale.sala, klasy.klasa, plan.id, plan.IdDzien, plan.IdGodzinaLekcyjna
 FROM slownik slownik, dni dni, godzinylekcyjne godzinylekcyjne, sale sale, klasy klasy, plan plan
-where plan.IdPrzedmiot = slownik.id and plan.IdDzien = dni.id and plan.IdGodzinaLekcyjna = godzinylekcyjne.id and plan.IdSala = sale.id and plan.IdKlasa = klasy.id and plan.IdKlasa = 3
+where plan.IdPrzedmiot = slownik.id and plan.IdDzien = dni.id and plan.IdGodzinaLekcyjna = godzinylekcyjne.id and plan.IdSala = sale.id and plan.IdKlasa = klasy.id
 ORDER BY plan.IdDzien , plan.IdGodzinaLekcyjna";
 	
 $rezultat = $polaczenie->query($lekcja);
@@ -69,7 +69,7 @@ if ($rezultat4->num_rows > 0)
 {
 while($wiersz = $rezultat4->fetch_assoc()) 
     {
-    echo '<option value="' . $wiersz["id"] . '">' . $wiersz['id'] . '.' . $wiersz["godzina"] . "</option>";
+    echo '<option value="' . $wiersz["id"] . '">' . $wiersz['id'] . '. ' . $wiersz["godzina"] . "</option>";
     }
 }
 echo '</select><select name="sala">';
@@ -124,25 +124,21 @@ if(isset($_POST['dodawanie']))
             }
         }
 
-if($czyMoznaWyslac)
-{
-		if($polaczenie->query($dodawanieLekcji))
-		{
-            header("Refresh:0");
-			
-		}else
-		{
-			echo "Nie udało się dodać lekcji!";
-		}
-}else if(!$czyMoznaWyslac)
-{
-    echo "O podanej godzinie podanego dnia dla podanej klasy już istnieje lekcja!";
-}
-		
-
-        
-    
-	
+    if($czyMoznaWyslac)
+    {
+            if($polaczenie->query($dodawanieLekcji))
+            {
+                header("Refresh:0");
+                
+            }else
+            {
+                echo "Nie udało się dodać lekcji!";
+            }
+    }
+    else if(!$czyMoznaWyslac)
+    {
+        echo "O podanej godzinie podanego dnia dla podanej klasy już istnieje lekcja!";
+    }	
 }
 
 $polaczenie->close();
