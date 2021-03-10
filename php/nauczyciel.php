097b25fd33e40e33d3b33350720c08f7468660dd
filipek625 <filip.piszczan@gmail.com>
2021-03-10 13:25:24 +0100

    sraka

--- php/nauczyciel.php
+++ php/nauczyciel.php
@@ -375,7 +375,7 @@ $polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
             ?>
         </div>
         <!-- Dodawanie przedmiotów -->
-        <form id="dodaj" method="post" action="nauczycielAkcje.php">
+        <form class="dodaj" method="post" action="nauczycielAkcje.php">
         
         <input type="text" name="dodawanyPrzedmiot">
         <input type="submit" name="dodawaniePrzedmiotu" value="Dodaj przedmiot!">
@@ -414,7 +414,7 @@ $polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
         </div>
         <!-- Dodawanie klasy -->
         
-        <form id="dodaj" method="post" action="nauczycielAkcje.php">
+        <form class="dodaj" method="post" action="nauczycielAkcje.php">
         <input  type="text" name="dodawanaKlasa">
         <input  type="submit" name="dodawanieKlasy" value="Dodaj klasę!">
         </form>
