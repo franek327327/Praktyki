7d11b4fd4dcc7df344f2ccf144c506bf0f27727e
filipek625 <filip.piszczan@gmail.com>
2021-03-10 13:14:41 +0100

    klasy przedmioty

--- php/nauczyciel.php
+++ php/nauczyciel.php
@@ -346,6 +346,7 @@ $polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
     <div class="tab-content" id="lekcje">
         <!-- Usuwanie i wyświetlanie przedmiotów-->
         <div class="wyswietlanie">
+        <h3>Dodaj Przedmiot</h3>
             <?php
                 $przedmioty = "SELECT id, przedmiot FROM slownik";
 
@@ -374,7 +375,8 @@ $polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
             ?>
         </div>
         <!-- Dodawanie przedmiotów -->
-        <form method="post" action="nauczycielAkcje.php">
+        <form id="dodaj" method="post" action="nauczycielAkcje.php">
+        
         <input type="text" name="dodawanyPrzedmiot">
         <input type="submit" name="dodawaniePrzedmiotu" value="Dodaj przedmiot!">
         </form>
@@ -411,10 +413,12 @@ $polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
         
         </div>
         <!-- Dodawanie klasy -->
-        <form method="post" action="nauczycielAkcje.php">
-        <input type="text" name="dodawanaKlasa">
-        <input type="submit" name="dodawanieKlasy" value="Dodaj klasę!">
+        
+        <form id="dodaj" method="post" action="nauczycielAkcje.php">
+        <input  type="text" name="dodawanaKlasa">
+        <input  type="submit" name="dodawanieKlasy" value="Dodaj klasę!">
         </form>
+        
     </div>
 </div>
 
